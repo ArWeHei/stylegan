dddb04f735b73cc85889e9ba0ed38d717b6e2840
ArWeHei <github@aheimbrecht.de>
2019-07-22 16:50:49 +0200

    add missing import

--- trainer.py
+++ trainer.py
@@ -12,6 +12,7 @@ from edflow.tf_util import make_linear_var
 
 from .loss import G_logistic_nonsaturating, D_logistic
 from .misc import process_reals
+import stylegan.ops as ops
 
 import tensorflow as tf
 import numpy as np
